8491ba086e049191c6fab35b13f585ab9aed8dd9
Alan Faulkner <Alan Faulkner@ALANS-VAIO>
2016-11-08 19:05:58 +0000

    Console version appears to work fine

--- Game_Of_Life/Main.cpp
+++ Game_Of_Life/Main.cpp
@@ -1,15 +1,14 @@
 /*Baisc implemtation of conways game of life
-  This uses a fixed size world 
+  This uses a fixed size world
   Inital posistion of life is hard coded
   print only to console window
   */
-
 
 #include <iostream>
 #include <vector>
 #include <random>
 
-//random number generator
+  //random number generator
 std::random_device rd;
 std::mt19937 generator(rd());
 std::uniform_int_distribution<int> distribution(0, 1);
@@ -17,9 +16,7 @@ std::uniform_int_distribution<int> distribution(0, 1);
 std::vector<std::vector<int>> world;
 std::vector<std::vector<int>> world_temp;
 
-
 int size_x, size_y, generations;
-
 
 int main()
 {
@@ -45,7 +42,7 @@ int main()
 		world.push_back(row);
 	}
 
-	//generate temp world to store next generation in 
+	//generate temp world to store next generation in
 	for (int i = 0; i < size_y; i++) {
 		std::vector<int> rowb;
 		for (int j = 0; j < size_x; j++) {
@@ -79,33 +76,30 @@ int main()
 		std::cin >> generations;
 	}
 
-	std::cout << std::endl << "Simulation has begun" << std::endl << "--------------------"<<std::endl<<std::endl;
+	std::cout << std::endl << "Simulation has begun" << std::endl << "--------------------" << std::endl << std::endl;
 
 	//Run simulation
 
-	
 	int Previous_population_total = 0;
 	int Population_2gena_ago = 0;
 
 	while (current_gen <= generations) {
-
 		int Total_Population = 0;
 
 		for (int i = 0; i < size_y; i++) {
 			for (int j = 0; j < size_x; j++) {
-
 				int number_of_cells = 0;
-				
+
 				//Conditional limits
 				int lower_limit_Y = i - 1;
 				int lower_limit_X = j - 1;
 				int upper_limit_Y = i + 1;
 				int upper_limit_X = j + 1;
-				
+
 				if (lower_limit_Y < 0) { lower_limit_Y = 0; }
 				if (lower_limit_X < 0) { lower_limit_X = 0; }
-				if (upper_limit_Y >= size_y) { upper_limit_Y = size_y-1; }
-				if (upper_limit_X >= size_x) { upper_limit_X = size_x-1; }
+				if (upper_limit_Y >= size_y) { upper_limit_Y = size_y - 1; }
+				if (upper_limit_X >= size_x) { upper_limit_X = size_x - 1; }
 				int testcount = 0;
 
 				//Calulate the number of live cells around the current one using limits.
@@ -115,17 +109,15 @@ int main()
 						testcount++;
 					}
 				}
-				
+
 				if (world[i][j] == 1) {
-					
 					number_of_cells--;
 					if (number_of_cells < 2) { world_temp[i][j] = 0; }
 					else if (number_of_cells == 2 || number_of_cells == 3) { world_temp[i][j] = 1; }
 					else if (number_of_cells > 3) { world_temp[i][j] = 0; }
 				}
-				
+
 				if (number_of_cells == 3) { world_temp[i][j] = 1; }
-				
 			}
 		}
 
@@ -160,19 +152,18 @@ int main()
 			std::cout << "The apocalypse has happend, all life has been wiped out within " << current_gen << " generations." << std::endl << std::endl << "Simuation Terminated!" << std::endl << "---------------------";
 			break;
 		}
-		
-		else if (Total_Population > Previous_population_total) { 
-			std::cout << "Population is increasing." << std::endl; 
+
+		else if (Total_Population > Previous_population_total) {
+			std::cout << "Population is increasing." << std::endl;
 		}
-		
-		else if (Total_Population < Previous_population_total) { 
-			std::cout << "Population is decreasing." << std::endl; 
+
+		else if (Total_Population < Previous_population_total) {
+			std::cout << "Population is decreasing." << std::endl;
 		}
-		
+
 		else if (Total_Population == Previous_population_total && Previous_population_total == Population_2gena_ago) {
 			std::cout << "The population is stagnet." << std::endl;
 		}
-
 
 		else if (current_gen == generations) {
 			std::cout << "Simulation has finished." << std::endl;
